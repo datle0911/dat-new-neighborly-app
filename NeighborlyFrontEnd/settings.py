004bce45a214e0edc2c8bda897ef6638cfcca8d3
Dat Le <thanhdatle.metal@gmail.com>
2024-06-18 08:40:29 +0700

    first commit

---
+++ NeighborlyFrontEnd/settings.py
@@ -0,0 +1,21 @@
+# --------- Flask settings
+SERVER_HOST = "localhost"  # Update this for the appropriate front-end website when up
+SERVER_PORT = 5000
+FLASK_DEBUG = False  # Do not use debug mode in prod
+
+# Flask-Restplus settings
+SWAGGER_UI_DOC_EXPANSION = "list"
+RESTPLUS_VALIDATE = True
+RESTPLUS_MASK_SWAGGER = False
+RESTPLUS_404_HELP = True
+API_VERSION = "v1"
+SCM_DO_BUILD_DURING_DEPLOYMENT = True
+
+# -------- Azure constants
+
+# API_URL format: "https://[FUNCTION_APP_NAME_GOES_HERE].azurewebsites.net"
+# API_URL = "https://neighborlyapi.azurewebsites.net/api/"
+
+# for local host if Azure functions served locally
+# API_URL = "http://localhost:7071/api"
+API_URL = "https://unique-function.azurewebsites.net/api"
---
+++ NeighborlyFrontEnd/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.fixture
+def client():
+    import neighborly
+
+    neighborly.app.config["TESTING"] = True
+    return neighborly.app.test_client()
